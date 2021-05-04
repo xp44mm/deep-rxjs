bcd86476d81af5e730292ab814166bc4cbb78233
xp44mm <34696643@qq.com>
2021-05-04 19:55:49 +0800

    observabledeep

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ export { deepCombineLatest } from './deepCombineLatest'
 export { deepMerge } from './deepMerge'
 export { isRxType } from './isRxType'
 export { ObservableArray } from './ObservableArray'
+export { observableDeep } from './observableDeep'
 export { pickBehaviorSubject } from './pickBehaviorSubject'
 export { restore } from './restore'
+
 
